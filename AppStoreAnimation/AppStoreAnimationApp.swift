961dc541cb91c08b5bdd4a747e358e9bc55b155c
Shen <863461783@qq.com>
2024-02-19 08:16:44 +0800

    Initial Commit

---
+++ AppStoreAnimation/AppStoreAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnimationApp.swift
+//  AppStoreAnimation
+//
+//  Created by 香饽饽zizizi on 2024/2/19.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
